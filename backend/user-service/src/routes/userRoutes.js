aa577d7db73d0599cd3e41d9fd10c5c9e3027886
NS <157737507+NipunSachintha@users.noreply.github.com>
2025-09-11 18:21:15 +0530

    Add/Update get sales staff details API

--- backend/user-service/src/routes/userRoutes.js
+++ backend/user-service/src/routes/userRoutes.js
@@ -4,10 +4,13 @@ const userController = require('../controllers/userController');
 const router = express.Router();
 
 // Routes
+
 router.get('/', userController.getAllUsers);
 //router.get('/profile', userController.getUserProfile);
+router.get('/sales-staff', userController.getSalesStaff);
 router.get('/:id', userController.getUserById);
 router.put('/:id', userController.updateUser);
 router.delete('/:id', userController.deleteUser);
+
 
 module.exports = router;
